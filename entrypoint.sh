1b4584b33bb67a2fb20006ec49905dc4e551eac5
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-14 13:30:39 -0300

    Enabled verbose output for kubectl

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ mkdir ~/.kube
 CLUSTER_ID=`curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters?name=${NODIS_CLUSTER_NAME} | jq -r .data[].id`
 curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > ~/.kube/kubesystem
 
-kubectl -v set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_FULL_VERSION}
+kubectl -v=6 set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_FULL_VERSION}
