08e41e8f6746d3a8bd40c172ef2e6853ddd3c01f
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-14 13:35:19 -0300

    Fixed kubeconfig file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 mkdir ~/.kube
 
 CLUSTER_ID=`curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters?name=${NODIS_CLUSTER_NAME} | jq -r .data[].id`
-curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > ~/.kube/kubesystem
+curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > ~/.kube/config
 
 kubectl -v=6 set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_FULL_VERSION}
