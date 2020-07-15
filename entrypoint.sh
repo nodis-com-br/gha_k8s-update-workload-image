fffec8092809ece76c3cd5df78644b7e747a8bc2
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-14 21:56:32 -0300

    fixed env vars names

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@
 mkdir ~/.kube
 
 CLUSTER_ID=`curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters?name=${NODIS_CLUSTER_NAME} | jq -r .data[].id`
-curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > ~/.kube/config
+curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > kubeconfig
 
-cat ~/.kube/config
-
-kubectl config get-contexts
-kubectl -v=6 set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_FULL_VERSION}
+kubectl -v=6 --kubeconfig=kubeconfig config get-contexts
+kubectl -v=6 --kubeconfig=kubeconfig set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_FULL_VERSION}
