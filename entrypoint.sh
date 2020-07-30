171fa304fa94db369b241c5d0ec9114a98067084
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-30 17:00:43 -0300

    Fixed kubectl command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ CLUSTER_ID=`curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters?name=${NO
 curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > kubeconfig
 
 kubectl -v=6 --kubeconfig=kubeconfig config get-contexts
-kubectl -v=6 --kubeconfig=kubeconfig set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_FULL_VERSION}
+kubectl -v=6 --kubeconfig=kubeconfig set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_IMAGE_NAME}:${NODIS_FULL_VERSION}
