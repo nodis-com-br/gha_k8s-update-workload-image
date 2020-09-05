912d5cf86f9097c8ccd531e1e966fc11cd586214
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-09-05 19:11:42 -0300

    Added scale to 1 if 0 command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ CLUSTER_ID=`curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters?name=${NO
 curl -s -u ${RANCHER_TOKEN} ${RANCHER_ENTRYPOINT}/clusters/${CLUSTER_ID}/?action=generateKubeconfig -X POST -H 'content-type: application/json' | jq -r .config > kubeconfig
 
 kubectl -v=6 --kubeconfig=kubeconfig config get-contexts
+kubectl -v=6 --kubeconfig=kubeconfig -n ${NODIS_K8S_NAMESPACE} scale --current-replicas=0 --replicas=1
 kubectl -v=6 --kubeconfig=kubeconfig -n ${NODIS_K8S_NAMESPACE} set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_IMAGE_NAME}:${NODIS_FULL_VERSION}
